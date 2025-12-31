824bcfd79090d0292a977a1030a6d4e8f145fc95
Víctor Enrique Aguirre <68919985+kikewebcity@users.noreply.github.com>
2025-12-31 00:40:28 +0000

    creacionimg

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,49 @@
 import React from 'react'
-import './App.css' // IMPORTANTE: Aquí debe estar importado el CSS donde pusiste el :root
+import './App.css'
+
+// 1. IMPORTA TUS IMÁGENES AQUÍ
+// Cambia los nombres de archivo por los que subiste
+import foto1 from './assets/banner.avif' 
+import foto2 from './assets/iconoreciclaje.png'
+import foto3 from './assets/iconotiempo.png'
+import foto4 from './assets/iconointerior.png'
+import foto5 from './assets/oficinap.jpg'
+import foto6 from './assets/tcalculadora.png'
+import foto7 from './assets/banop.jpg'
+// Si tienes más, sigue importando: import foto3 from ...
 
 function App() {
   return (
-    // Aplicamos la variable directamente aquí para probar
-    <div style={{ fontFamily: 'var(--font-main)', textAlign: 'center', marginTop: '50px' }}>
+    <div className="portfolio-container">
       
-      <h1>PRUEBA DE FUENTE TEKO</h1>
-      
-      <p style={{ fontSize: '1.5rem' }}>
-        Si ves este texto estilizado (alto y cuadrado), <br />
-        tu variable --font-main está funcionando correctamente.
-      </p>
+      <header>
+        <h1>MI PORTAFOLIO</h1>
+        <p className="subtitle">UX DESIGNER | CYBERPUNK WRITER</p>
+      </header>
 
-      <button style={{ fontFamily: 'var(--font-main)', fontSize: '1.2rem', padding: '10px 20px' }}>
-        Botón de prueba
-      </button>
+      <div className="gallery-grid">
+        
+        {/* TARJETA 1 */}
+        <div className="card">
+          <div className="image-box">
+             <img src={foto1} alt="Proyecto 1" />
+          </div>
+          <h2>NOMBRE PROYECTO 1</h2>
+          <p>Descripción breve del proyecto (ej. Novela).</p>
+        </div>
 
+        {/* TARJETA 2 */}
+        <div className="card">
+          <div className="image-box">
+             <img src={foto2} alt="Proyecto 2" />
+          </div>
+          <h2>NOMBRE PROYECTO 2</h2>
+          <p>Descripción breve (ej. App Bricko).</p>
+        </div>
+
+        {/* Puedes copiar y pegar más bloques "div className=card" aquí */}
+
+      </div>
     </div>
   )
 }
